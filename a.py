273343ce02ff21c523fd93693fb7e4ac29449aa7
TaskerJ72129 <60583303+TaskerJ72129@users.noreply.github.com>
2021-07-05 10:14:00 +1200

    .

--- a.py
+++ a.py
@@ -2,31 +2,3 @@ s = "$3.00"
 fixed = s[1:]
 print(fixed)
 
-a = 0
-d = variable_frame.iat[a,0]
-print(d)
-a += 1
-d = variable_frame.iat[a,0]
-print(d)
-s = d[1:]
-print(s)
-c = (float(s))
-
-while c > budget:
-    a += 1
-
-
-=======
-a = 0
-s = variable_frame.iat[a,0]
-print(type(s))
-c = (float(s))
-print(type(c))
-
-while c > budget:
-    a += 1
-    print(a)
-    
->>>>>>> 897f0861b7b800ee9f343ae2ee2e9d8cbe3468ac
-# recommendation
-recommendation = variable_frame.iloc[[a]]
