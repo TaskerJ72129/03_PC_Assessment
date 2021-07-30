d660bc7eef87f52b554de833183d04c71148558d
TaskerJ72129 <60583303+TaskerJ72129@users.noreply.github.com>
2021-07-30 12:28:24 +1200

    a

--- 05_g_or_kg_V2.py
+++ 05_g_or_kg_V2.py
@@ -3,19 +3,21 @@ valid = False
 while not valid:
 
     # ask for profit goal...
-    weight = input("weight(g):")
+    weight = input("weight(g or kg):")
 
     # check if first character is $...
     if weight[-1] == "g":
         kg_or_g = "g"
-        # Get amount (everything before the g)
-        amount = weight[1:]
+        # Get amount (everything before the kg)
+        amount = weight[:-1]
+
         valid = True
     # check if the last character is %
     elif weight [-1] == "kg":
         kg_or_g = "kg"
-        # Get amount (everything before the kg)
-        amount = weight[:-1]
+        weight_kg = (float(weight))
+
+        
         Valid = True
 
     else:
@@ -23,7 +25,10 @@ while not valid:
         
 
 weight = weight[:-1]
+weight_g = (float(weight))
+
 
 print(kg_or_g)
 print(weight)
-
+print(weight_g)
+print(weight_kg)
--- random_testing.py
+++ random_testing.py
@@ -1,3 +1,6 @@
 s = "$3.00"
 fixed = s[1:]
 print(fixed)
+
+# Get amount (everything before the kg)
+amount = weight[:-1]
