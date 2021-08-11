34cb4081f07e48be68e410e1b99e4e40b0be6f4d
TaskerJ72129 <60583303+TaskerJ72129@users.noreply.github.com>
2021-08-11 15:10:15 +1200

    pls

--- 05_g_or_kg_V3.py
+++ 05_g_or_kg_V3.py
@@ -1,4 +1,23 @@
 
+# Checks that the user has entered yes / no to a question
+def yes_no(question):
+
+    to_check = ["yes", "no"]
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+
+        print("Please enter either yes or no...\n")
+
+
 valid = False 
 while not valid:
     try:
@@ -7,15 +26,16 @@ while not valid:
 
         # check if the last 2 characters are kg
         if weight[-2:] == "kg":
+            type = "kg"
             amount = weight[:-2]
             weight_kg = (float(amount))
             weight_g = weight_kg * 1000
-
 
             valid = True
 
         # if its not kg check if last character is g
         elif weight[-1] == "g":
+            type = "g"
             # Get amount (everything before the g)
             amount = weight[:-1]
             weight_g = (float(amount))
@@ -24,7 +44,16 @@ while not valid:
             valid = True
 
         else:
-            print("Please enter _kg or _g")
+            amount = weight
+            weight = (float(amount))
+            type = "unknown"
+
+        if type == "unknown" and amount <= 5:
+            type = yes_no("Do you mean {:.2f}kg"
+                          "? , y / n ".format(amount, amount))
+            
+    
+
 
     except ValueError:
         print("Please enter _kg or _g")
